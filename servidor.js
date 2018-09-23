66a835b93e04522b8aa435f03ed7aa9b5c2a5556
Luis Plata <www.luisplata@gmail.com>
2018-09-23 18:53:38 -0500

    preparando la aip

--- servidor.js
+++ servidor.js
@@ -1,9 +1,40 @@
-var http = require("http"),
-fs = require("fs");
+var express = require("express");
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+var app = express();
 
-var servidor = http.createServer(function(solicitud, responder){
+mongoose.connect("mongodb://localhost/api");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//esquemas
+var usuarios={
+	nombre:String,
+	pass:String
+};
+
+var esquemaUsuario = mongoose.Schema(usuarios);
+
+var Usuario = mongoose.model("Usuario",esquemaUsuario);
+
+app.get("/login",function(req,res){
+	Usuario.find(function(err,doc){
+		console.log(doc);
+		//var completo = req.params.nombre+"-"+req.params.contrasenia;
+		res.send(doc);
+	});
 	
-	responder.end("hola");
+});
+app.post("/login",function(req,res){
+	var completo = req.body.nombre+"-"+req.body.pass;
+	console.log(req.body.nombre);
+	console.log(req.body.pass);
+	var usuario = new Usuario({nombre:req.body.nombre,pass:req.body.pass});
+	usuario.save(function(){
+		res.send(usuario.nombre+"+"+usuario.pass);
+	});
+	
 });
 
-servidor.listen("80");
+app.listen(80);
